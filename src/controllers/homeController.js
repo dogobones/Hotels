c928d10bb7e9e03b0dafc940b12dd233582b0d6f
Raul Chan <endlave19@gmail.com>
2019-09-26 11:06:32 -0500

    Añadir sitios
    -Ya se puede subir archivos.

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,25 +3,34 @@ const pool = require(path.join('..','db'));
 
 const controller = {};
 controller.createsite = async (req, res) => {
-try {
-console.log("hola");
+
 var imagen="";
-console.log(req.files.LogoFile);
-console.log(req.files.MapFile);
- if(req.files.file!=undefined) {
+//console.log(req.files.LogoFile);
+console.log(req);
 
-   let file = req.files.file;
-
-   file.mv(`.//src//public//img//${file.name}`);
-
-   imagen=file.name;
-
+if(req.files!=undefined){
+ if(req.files.LogoFile!=undefined) {
+//console.log(req.files.LogoFile);
+  var fileLogo = req.files.LogoFile;
+   fileLogo.mv(`./src/public/img/${fileLogo.name}`);
+   rutaLogo=`/src/public/img/${fileLogo.name}`;
  }
 
-    //const hotel = await pool.query("SELECT * FROM Hoteles WHERE id = 1");
-  } catch(err) {
-    console.log(err);
+ if(req.files.MapFile!=undefined) {
+ //console.log(req.files.MapFile);
+    var fileMap = req.files.MapFile;
+    fileMap.mv(`./src/public/img/${fileMap.name}`);
+    rutaMapa=`/src/public/img/${fileLogo.name}`;
   }
+}
+let nombreSitio = req.body.nombreSitio;
+//console.log(nombreSitio);
+const query = await pool.query("INSERT INTO Hoteles(nombre,mapa,logo) values(?,?,?)",[nombreSitio,rutaLogo,rutaMapa]);
+const hotel = await pool.query("SELECT * FROM Hoteles WHERE id = ?",[query.insertId]);
+
+res.redirect('/editsite/:hotelId');
+
+
 
 };
 module.exports = controller;
