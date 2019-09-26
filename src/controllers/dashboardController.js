62fa0a905866e2e2f8ffbc52c03acd76c7f35a16
Unknown <jrsierrahernandez@gmail.com>
2019-09-26 09:24:07 -0500

    Dinamic Map

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -4,20 +4,10 @@ const pool = require(path.join('..','db'));
 const controller = {};
 
 controller.editsite = async (req, res) => {
-  try {
     const hotel = await pool.query("SELECT * FROM Hoteles WHERE id = 1");
-    console.log(hotel);
-  } catch(err) {
-    console.log(err);
-  }
-  res.render('../views/dashboard/editsite', {
-     hotel: {
-       "left": 15,
-       "top": 15,
-       "width": 15,
-       "height": 15,
-     }
-  });
+    res.render('../views/dashboard/editsite', {
+       hotel: hotel[0]
+    });
 };
 
 module.exports = controller;
